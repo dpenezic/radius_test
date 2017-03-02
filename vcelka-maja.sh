f630a9bb95445cd8ec2e1d633753da34f7a4cf34
Jan Tomasek <jan@tomasek.cz>
2017-03-02 12:57:44 +0100

    added support for MAC param & fixed PEAP

--- vcelka-maja.sh
+++ vcelka-maja.sh
@@ -7,29 +7,35 @@ HOSTNAME=$3
 SECRET=$4
 REALM=$5
 
-output=`/usr/local/rad_eap_test/rad_eap_test -H $HOSTNAME -P 1812 -S $SECRET -m WPA-EAP -e TTLS -u $ID -p $PASS -t 25 -A vcelka-maja@$REALM 2>/dev/null`
-code=$?
-if [ $code -eq 0 ]
+MAC1=$6
+MAC2=$7
+
+output=`/usr/local/rad_eap_test/rad_eap_test -H $HOSTNAME -P 1812 -S $SECRET -m WPA-EAP -e TTLS -u $ID -p $PASS -t 25 -A vcelka-maja@$REALM -M $MAC1 2>/dev/null`
+ttls_code=$?
+ttls_out="TTLS: vcelka-maja@$REALM $output"
+if [ $ttls_code -eq 0 ]
 then
-  # test byl uspesny
-  echo "TTLS: vcelka-maja@$REALM $output"
+  # test byl uspesny, nema smysl to dal testovat
+  echo $ttls_out
   exit 2;
 else
-  output=`/usr/local/rad_eap_test/rad_eap_test -H $HOSTNAME -P 1812 -S $SECRET -m WPA-EAP -e TTLS -u $ID -p $PASS -t 25 -A vcelka-maja@$REALM 2>/dev/null`
-  code=$?
-  if [ $code -eq 0 ]
+  output=`/usr/local/rad_eap_test/rad_eap_test -H $HOSTNAME -P 1812 -S $SECRET -m WPA-EAP -e PEAP -u $ID -p $PASS -t 25 -A vcelka-maja@$REALM -M $MAC2 2>/dev/null`
+  peap_code=$?
+  peap_out="PEAP: vcelka-maja@$REALM $output"
+  if [ $peap_code -eq 0 ]
   then
-  # test byl uspesny
-    echo "PEAP: vcelka-maja@$REALM $output"
+    # test byl uspesny
+    echo $peap_out;
+    exit 2;
   fi 
 fi
 
-if [ $code -eq 1 ]
+if [ $ttls_code -eq 1 ] || [ $peap_code -eq 1 ]
 then
   # reject
-  echo "TTLS,PEAP: vcelka-maja@$REALM $output: this is OK";
+  echo "$ttls_out; $peap_out; this is OK";
   exit 0
 fi
 
-echo "maja $output: problem?";
+echo "$ttls_out; $peap_out; problem?"
 exit 1;
